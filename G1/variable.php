c51412648c3e58bd9dac75e2752a8aeb2170dfc1
Alvee TaniYa <115446439+taniya-alvi@users.noreply.github.com>
2022-11-14 11:44:04 +0600

    Add files via upload

---
+++ G1/variable.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+Taniya
+
+
+<?php
+$num1=40;
+$num2=80;
+?>
+
+
+<h1>Taniya</h1>
+<div class="test">
+<?php
+echo "This is my first program in PHP";
+
+
+$result=$num1+$num2;
+echo'<br>'.$result
+
+?>
+</div>
+<div class="side">
+    <h4>IIST</h4>
+</div>
+
+
+    
+</body>
+</html>
